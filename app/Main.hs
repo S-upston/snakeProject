dd638fda49e9f92f48284c5b337b26a5f90af0eb
S-upston <98416592+S-upston@users.noreply.github.com>
2024-12-08 15:20:44 -0700

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -116,48 +116,27 @@ update _ gameState = case screen gameState of
 
 updateGame :: GameState -> IO GameState
 updateGame gameState
-  | not (alive gameState) = return gameState { screen = GameOver }
+  | not (alive gameState) = return gameState { screen = GameOver, leaderboard = updateLeaderboard (score gameState) (leaderboard gameState) }
   | otherwise = do
       let newHead = move (dir gameState) (head (snake gameState))
-          newDuoHead = move (oppositeDirection (dir gameState)) (head (duoSnake gameState))  -- Duo snake moves in the opposite direction
-
-          -- Update snake with check for Tail Mode
-          newSnake = if tailMode gameState && newHead == food gameState
-                     then if length (snake gameState) == 1  -- If snake has only one segment
-                          then [newHead]  -- Don't call `tail` on a single segment
-                          else tail (snake gameState) ++ [newHead]  -- Call `tail` only if there are more than 1 segment
-                     else if newHead == food gameState
-                     then newHead : snake gameState
-                     else newHead : init (snake gameState)
-
-          -- Update duoSnake with check for Tail Mode
-          newDuoSnake = if duoMode gameState
-                         then if tailMode gameState && newDuoHead == food gameState
-                              then if length (duoSnake gameState) == 1
-                                   then [newDuoHead]  -- Don't call `tail` on a single segment
-                                   else tail (duoSnake gameState) ++ [newDuoHead]
-                              else if newDuoHead == food gameState
-                                   then newDuoHead : duoSnake gameState
-                                   else newDuoHead : init (duoSnake gameState)
-                         else []  -- If Duo Mode is disabled, duoSnake should be empty
-
-      -- Separate collision checks for the main snake and the duo snake
-      let snakeCollides = collision newHead newSnake || collision newHead (walls gameState)
-          duoSnakeCollides = if duoMode gameState then collision newDuoHead newDuoSnake || collision newDuoHead (walls gameState) else False
-
-      -- If either snake collides, the game is over
-      if snakeCollides || duoSnakeCollides
+          newSnake
+            | tailMode gameState && newHead == food gameState = -- Tail mode logic
+                -- When in Tail Mode and the snake eats food, the tail becomes the new head
+                (tail (snake gameState)) ++ [newHead]
+            | newHead == food gameState = -- Regular snake eating food
+                newHead : snake gameState
+            | otherwise = -- Regular snake movement
+                newHead : init (snake gameState)
+      if collision newHead (newSnake ++ walls gameState)
         then return gameState { alive = False, screen = GameOver }
         else do
           newFood <- if newHead == food gameState then randomFoodPosition else return (food gameState)
           return gameState
             { snake = newSnake
-            , duoSnake = newDuoSnake  -- Update duoSnake
             , food = newFood
             , score = if newHead == food gameState then score gameState + 1 else score gameState
             , hiScore = max (score gameState + 1) (hiScore gameState)
             }
-
 
 
 
