0e2a5ab4b945faed7279a2c7d2885adce65a609b
S-upston <98416592+S-upston@users.noreply.github.com>
2024-12-08 14:58:13 -0700

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -52,7 +52,7 @@ renderStartScreen = pictures
   ]
 
 renderGameScreen :: GameState -> Picture
-renderGameScreen (GameState snake _ food _ score hiScore _ walls _ _ duoSnake _) = pictures $
+renderGameScreen (GameState snake _ food _ score hiScore _ walls _ duoMode duoSnake _) = pictures $
   [ translateBlock pos (color green (rectangleSolid size size)) | pos <- snake ] ++
   (if duoMode then [ translateBlock pos (color blue (rectangleSolid size size)) | pos <- duoSnake ] else []) ++
   [ translateBlock pos (color white (rectangleSolid size size)) | pos <- walls ] ++
