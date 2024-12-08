9f67445e65a8bfc7ea20acae9e3b13e88a94cdb4
S-upston <98416592+S-upston@users.noreply.github.com>
2024-12-08 14:42:40 -0700

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -93,7 +93,7 @@ initialState = do
     , walls = []
     , tailMode = False
     , duoMode = False
-    , duoSnake = [(0,4),(-1,4),(-2,4),(-3,4)]
+    , duoSnake = []
     , leaderboard = []
     }
     
