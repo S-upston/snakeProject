6a47e2cc46de3729045fc25f78d1b2025f6273b3
S-upston <98416592+S-upston@users.noreply.github.com>
2024-12-08 18:57:59 -0700

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -46,9 +46,9 @@ render gameState = case screen gameState of
 renderStartScreen :: Picture
 renderStartScreen = pictures
   [ translate (-130) 100 (scale 0.2 0.2 (color white (text "Press ENTER to Start")))
-  , translate (-150) 50 (scale 0.2 0.2 (color white (text "Use ARROW KEYS to move")))
-  , translate (-150) 0 (scale 0.2 0.2 (color white (text "Press T to toggle Tail Mode")))
-  , translate (-150) (-100) (scale 0.2 0.2 (color white (text "Press 1,2,3,4,5 to select level")))
+  , translate (-175) 50 (scale 0.2 0.2 (color white (text "Use ARROW KEYS to move")))
+  , translate (-175) 0 (scale 0.2 0.2 (color white (text "Press T to toggle Tail Mode")))
+  , translate (-175) (-100) (scale 0.2 0.2 (color white (text "Press 1,2,3,4,5 to select level")))
   ]
 
 renderGameScreen :: GameState -> Picture
